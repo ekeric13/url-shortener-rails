3b1ee263bd56e1ad7820b62b465c49720a7f8012
Antonio Dangond & Eric Kennedy <pair+antoniodangond.ekeric13@devbootcamp.com>
2014-08-28 12:23:11 -0700

    made our javascript oo

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -3,17 +3,24 @@ $(document).on("page:change", function(){
   //   $('.test').css(("color", "green"))
   // })
   if ($("#profilePage").length > 0) {
+    function Color() {
 
-    var makeRed = function() {
-    $(this).css("color", "red")
     }
-    $('.test').on('mouseover', makeRed);
-
-    var test = document.getElementsByClassName('test')[0];
-    var blue = function() {
-      this.style.color = 'blue'
-    };
-    test.addEventListener("click", blue)
+    Color.prototype = {
+      makeRed: function() {
+      $(this).css("color", "red")
+      },
+      blue: function() {
+        this.style.color = 'blue'
+      },
+      bindEventListeners: function() {
+        $('.test').on('mouseover', this.makeRed);
+        var test = document.getElementsByClassName('test')[0];
+        test.addEventListener("click", this.blue);
+      }
+    }
+    color = new Color();
+    color.bindEventListeners();
   };
 
 
